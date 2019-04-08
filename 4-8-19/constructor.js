834ee57c9d7bef1aa39911b233ddcb034cd3a357
batti19 <tiquilamcneill@gmail.com>
2019-04-08 09:38:43 -0400

    new files

---
+++ 4-8-19/constructor.js
@@ -0,0 +1,10 @@
+function particle(startX, startY){
+     this.x = startX;
+     this.y = startY
+     velocity = {x:0, y:0}
+}
+particles[]{
+for (x= 0, x <100, x++){
+    y =(Math.random()*500);
+}
+}
