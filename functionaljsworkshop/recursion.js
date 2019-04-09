85e12589c001949baf8a65ac0855942668a7a95c
batti19 <tiquilamcneill@gmail.com>
2019-04-09 09:31:58 -0400

    rps update

--- functionaljsworkshop/recursion.js
+++ functionaljsworkshop/recursion.js
@@ -5,7 +5,12 @@ reduce([1,2,3], function(prev, curr, index, arr) {
 
 function reduce(arr, fn, initial) {
     // SOLUTION GOES HERE
-    
+
+            console.log( accumulator);
+            console.log( 'currentValue: ' + currentValue);
+            console.log( 'currentIndex: ' + currentIndex);
+            console.log( 'array: ' + array + "\n");
+
 }
   
   module.exports = reduce
