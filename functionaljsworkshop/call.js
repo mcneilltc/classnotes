f106a67024a9a0063cc9d7857984ef40ebca46b7
batti19 <tiquilamcneill@gmail.com>
2019-04-10 16:47:02 -0400

    ?

--- functionaljsworkshop/call.js
+++ functionaljsworkshop/call.js
@@ -31,7 +31,7 @@ object.hasOwnProperty('quack')
 var notDuck = Object.create({quack: true})
     var duck = {quack: true}
     duckCount(duck, notDuck) // 1
-
+//computer answer
 function duckCount() {
    return Array.prototype.slice.call(arguments).filter(function(obj){
        return Object.prototype.hasOwnProperty.call(obj, 'quack')
