dcec0b40d211104a37ace6e1c5dab30dc5f492cb
batti19 <tiquilamcneill@gmail.com>
2019-04-10 10:35:37 -0400

    changes

--- 4-10-19/selnodes.js
+++ 4-10-19/selnodes.js
@@ -1,15 +1,21 @@
-const firstItem= document.querySelector('li.first');
-const header = document.firstItem.parentElement;
-//console.log(header);
-const section = document.firstItem.parentElement.parentElement.querySelector(nextElementSibling);
-//console.log(section);
-const curr = document.section.nextElementSibling;
-//console.log(curr);
-const afterCur = documment.curr.nextElementSibling;
-//console.log(afterCur)
-const h2 = document.curr.previousElementSibling;
-//console.log(h2);
-const highlight = document.header.nextElementSibling;
-//console.log(highlight)
-const allH2 = document.highlight.querySelector(h2);
-//console.log(allH2);
+const head= document.querySelector('header');
+// const header = document.firstItem.parentElement;
+console.log(head);//header with all items
+
+const section = head.nextElementSibling.querySelector('col');
+console.log(section);//sections in div with all the headers
+
+const curr = section.querySelector('current');
+console.log(curr);// section labeled current section 2 highlighted
+
+const afterCur = curr.nextElementSibling;
+console.log(afterCur)// section after current with section 2
+
+const h2 = curr.previousElementSibling.children;
+console.log(h2);// should log section 1
+
+const highlight = section.children.querySelector('highlight');
+console.log(highlight)//get the section with h2 with highlight
+
+const allH2 = section.children.querySelector('h2');
+console.log(allH2);// all the h2 tags
