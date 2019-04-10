0260fc77eaae6b458eba26243b2b5e3609adbe40
batti19 <tiquilamcneill@gmail.com>
2019-04-10 09:54:03 -0400

    nodes

---
+++ 4-10-19/selnodes.js
@@ -0,0 +1,15 @@
+const firstItem= document.querySelector('li.first');
+const header = document.firstItem.parentElement;
+//console.log(header);
+const section = document.firstItem.parentElement.parentElement.querySelector(nextElementSibling);
+//console.log(section);
+const curr = document.section.nextElementSibling;
+//console.log(curr);
+const afterCur = documment.curr.nextElementSibling;
+//console.log(afterCur)
+const h2 = document.curr.previousElementSibling;
+//console.log(h2);
+const highlight = document.header.nextElementSibling;
+//console.log(highlight)
+const allH2 = document.highlight.querySelector(h2);
+//console.log(allH2);
