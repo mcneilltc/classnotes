00aaea7b8f98a88ce123ca76358a0d104193c835
batti19 <tiquilamcneill@gmail.com>
2019-04-12 10:32:15 -0400

    button 5

--- 4-12-19/rest.js
+++ 4-12-19/rest.js
@@ -13,7 +13,19 @@ $('#getallposts').click(function(){
 	});//the button will get all the post
 });
 
-$('#post10').click(function(){$.get('http://jsonplaceholder.typicode.com/comments?postId=10', function(postId){
-    console.log(postId);
+$('#post10').click(function(){$.get('http://jsonplaceholder.typicode.com/posts/10', function(postId){
+    console.log(postId);//1 user with the post id of 10
+});
+});
+$('#com12').click(function(){$.get('http://jsonplaceholder.typicode.com/comments?postId=12', function(comId12){
+    console.log(comId12);// comments with the post id of 12
+});
+});
+$('#userid2').click(function(){$.get('http://jsonplaceholder.typicode.com/users?userId=2', function(userId2){
+    console.log(userId2);// users with the user id of 2
+});
+});
+$('#newpost').click(function(){$.get('http://jsonplaceholder.typicode.com/users?userId=2', function(newPost){
+    console.log(newPost);// create a new post with new id
 });
 });
