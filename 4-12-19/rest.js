3ea3789322fd0146ef318246f8d900526d502028
batti19 <tiquilamcneill@gmail.com>
2019-04-12 12:38:48 -0400

    last button

--- 4-12-19/rest.js
+++ 4-12-19/rest.js
@@ -1,10 +1,4 @@
-
-
-//Make a GET request for the items to render
-/*$.get('http://jsonplaceholder.typicode.com/posts',function(posts){
-	//Iterate over the response, adding elements to DOM
-	console.log(posts);
-	});//get the post*/
+//get all posts
 
 $('#getallposts').click(function(){
     $.get('http://jsonplaceholder.typicode.com/posts',function(posts){
@@ -12,26 +6,27 @@ $('#getallposts').click(function(){
 	console.log(posts);
 	});//the button will get all the post
 });
-
+//get post id of 10
 $('#post10').click(function(){$.get('http://jsonplaceholder.typicode.com/posts/10', function(postId){
     console.log(postId);//1 user with the post id of 10
 });
 });
+//get the comments from post with id of 12
 $('#com12').click(function(){$.get('http://jsonplaceholder.typicode.com/comments?postId=12', function(comId12){
     console.log(comId12);// comments with the post id of 12
 });
 });
-//userid
+//get all the posts from the user with the id of 2
 $('#userid2').click(function(){$.get('http://jsonplaceholder.typicode.com/users?userId=2', function(userId2){
     console.log(userId2);// users with the user id of 2
 });
 });
-//create new post
+//create new post and log the id generated for it by the server
 $('#newpost').click(function(){$.post('http://jsonplaceholder.typicode.com/posts/', function(newPost){
-    console.log(newPost);// create a new post with new id and it is supplied with an id
+    console.log(newPost.id);// create a new post with new id and it is supplied with an id
 });
 });
-//replace post
+//replace post the post with id of 12 and render the responseJSON
 $('#replacepost').click(function(){
 fetch('https://jsonplaceholder.typicode.com/posts/12',{
     method: 'PUT',
@@ -58,12 +53,13 @@ fetch('https://jsonplaceholder.typicode.com/posts/12',{
   .then(response => response.json())
   .then(json => console.log(json))
 })
-//update post
-fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//update the title of the posr with id of 12 and render responseJSON
+$('#updatepost').click(function(){
+fetch('https://jsonplaceholder.typicode.com/posts/12', {
     method: 'PUT',
     body: JSON.stringify({
       id: 1,
-      title: 'foo',
+      title: 'all of this work',
       body: 'bar',
       userId: 1
     }),
@@ -73,3 +69,33 @@ fetch('https://jsonplaceholder.typicode.com/posts/1', {
   })
   .then(response => response.json())
   .then(json => console.log(json))
+
+  fetch('https://jsonplaceholder.typicode.com/posts/12', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: 'almost finished',
+     userId: 1// had to add user Id in order to get it to stay the same
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }  
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+})
+  //delete post
+  
+  $('#deletepost').click(function(){fetch('https://jsonplaceholder.typicode.com/posts/12', {
+    method: 'DELETE'
+    
+  })
+  alert('Successfully deleted')})
+
+  //display a list of posts, when the user clicks on the post, display all the comments from the post and then display a link back to all posts
+
+$('display').click(function(){$.get('http://jsonplaceholder.typicode.com/posts',function(posts){
+	
+	console.log(posts).innerHTML;
+	});
+
+})
