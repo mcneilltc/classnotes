bbf8828975e93843701bd48b17560b584fbdab91
batti19 <tiquilamcneill@gmail.com>
2019-04-18 11:32:19 -0400

    new fizz

---
+++ 4-18-19/fizztest.js
@@ -0,0 +1,7 @@
+let n =1
+for (n; n<=100; n++){
+    if (n%15 ===0 )
+    console.log("fizzbuzz");
+else if (n%5 ===0) console.log("buzz");
+else if (n%3 === 0) console.log("fizz");
+else console.log(n)}
